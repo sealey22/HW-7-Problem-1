0ee24d9621da40da9bfe9dfb97061c22074fae3a
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-10-08 01:09:46 +0000

    layout with errors

--- .ccls-cache/@home@runner@HW-7-Problem-1/main.cpp
+++ .ccls-cache/@home@runner@HW-7-Problem-1/main.cpp
@@ -1,5 +1,51 @@
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 
+using namespace std;
+
+/* user defined function identifier */
+void seconds(int, int &, int &, int &);
+
 int main() {
-  std::cout << "Hello World!\n";
+
+  int secs;
+  int mins;
+  int hours;
+  int second;
+
+  /* cout << " Enter number of seconds: ";
+   cin >> secs;
+   cout << " Enter number of minutes: ";
+   cin >> mins;
+   cout << " Enter number of hours: ";
+   cin >> hours; */
+  double sec;
+  cout << "enter number of seconds: ";
+  cin >> sec;
+
+  seconds(hours, mins, secs, second);
+
+  cout << hours << endl;
+  cout << secs << endl;
+  cout << mins << endl;
+  cout << second << endl;
+
+  return 0;
 }
+
+/* user defined function */
+void seconds(int hours, int mins, int secs, int second) {
+
+  int time;
+
+  time = (seconds % 3600);
+
+  hours = seconds / 60;
+
+  mins = time / 60;
+
+  secs = time % 60;
+
+  time = sec;
+};
